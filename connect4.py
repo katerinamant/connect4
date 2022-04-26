b5e487ea654b81954497dbfd0ae06cfb96ef71f1
Katerina Mantaraki <97695022+katerinamant@users.noreply.github.com>
2022-04-27 01:53:19 +0300

    Update connect4.py

--- connect4.py
+++ connect4.py
@@ -1,18 +1,3 @@
-"""
-Game of Connect 4!
-
-The players choose the columns of the board.
-
-The player chooses where to place a piece.
-The player gets 4 points for a win.
-They get an extra point for every neighboring piece of theirs.
-The winning pieces are removed and the board is moved accordingly.
-Afterwards the board is checked again for more wins and, if there are any, the process is repeated.
-The process stops when no wins are found and it's the next player's turn.
-
-The players have the possibility to save their progress to a CSV file.
-The game ends when the board is full.
-"""
 import csv
 import os
 
